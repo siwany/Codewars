14beb6f769917266be326ee7e016fb65a4bacd6f
Siwan Yang <123605713+siwany@users.noreply.github.com>
2023-04-19 16:58:54 -0400

    Create Keep Hydrated!.java

---
+++ 8kyu/Keep Hydrated!.java
@@ -0,0 +1,10 @@
+public class KeepHydrated  {
+
+  public int Liters(double time)  {
+    
+    //Your code goes here! Hint: You should change that -1
+    return Math.floorDiv((int)time,2);
+  
+  }
+
+}
