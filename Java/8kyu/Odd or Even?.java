77c654d652d39e69b334b61824300adc015232b9
Siwan Yang <123605713+siwany@users.noreply.github.com>
2023-04-20 14:58:00 -0400

    Delete Odd or Even?.java

--- Java/8kyu/Odd or Even?.java
+++
@@ -1,9 +0,0 @@
-public class Codewars {
-  public static String oddOrEven (int[] array) {
-    int sum = 0;
-    for (int n : array){
-      sum += n;
-    }
-    return sum % 2==0 ? "even" : "odd";
-  }
-}
